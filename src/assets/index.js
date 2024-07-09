5741ff0f55415c8aa62168a484d37bebbc308704
ajnaskp67 <ajnas@akiraplc.com>
2024-07-09 20:42:43 +0530

    create product section, weHelp ,why chooseus a component

---
+++ src/assets/index.js
@@ -0,0 +1,35 @@
+// import import Background from './bg/background.jpg';
+import Bag from "./images/bag.svg";
+import Sofa from "./images/couch.png";
+import Product1 from "./images/product-1.png";
+import Product2 from "./images/product-2.png";
+import Product3 from "./images/product-3.png";
+import CrossIcon from "./images/cross.svg";
+import TruckIcon from "./images/truck.svg";
+import BagIcon from "./images/bag.svg";
+import SupportIcon from "./images/support.svg";
+import ReturnIcon from "./images/return.svg";
+import ChooseImgageUs from "./images/why-choose-us-img.jpg";
+import Grid1 from "./images/img-grid-1.jpg";
+import Grid2 from "./images/img-grid-2.jpg";
+import Grid3 from "./images/img-grid-3.jpg";
+
+// import Second from './photos/second.jpg';
+// import LinkedIn from './social/linkedin.png';
+
+export {
+  Bag,
+  Sofa,
+  Product1,
+  Product2,
+  Product3,
+  CrossIcon,
+  TruckIcon,
+  BagIcon,
+  SupportIcon,
+  ReturnIcon,
+  ChooseImgageUs,
+  Grid1,
+  Grid2,
+  Grid3,
+};
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from 'react';
+
 import Header from '../../components/Header'
 import Hero from '../../components/Hero'
+import ProductSection from '../../components/ProductSection';
+import WhyChooseUs from '../../components/WhyChooseUs';
+import WeHelp from '../../components/WeHelp'
 
 const HomePage = () => {
   return (
     <div>
 		<Header/>
 		<Hero/>
+		<ProductSection/>
+		<WhyChooseUs/>
+    <WeHelp/>
       
     </div>
   )
